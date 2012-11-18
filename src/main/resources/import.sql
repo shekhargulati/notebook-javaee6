01a46d2e7ea966b0b79ef6ac0a87b9f19fc6cf2e
Shekhar Gulati <shekhargulati84@gmail.com>
2012-11-18 08:55:49 +0530

    updated import.sql with Notebook table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+insert into Notebook (id, name, description, createdOn,owner) values (0, 'OpenShift Notebook', 'This is a notebook containing notes about OpenShift', CURRENT_DATE(),'Shekhar Gulati') 
